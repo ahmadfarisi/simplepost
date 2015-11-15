6e405fd8b6c4f2720d2df0ea29daa0aeebcaad78
ahmadfarisi <ahmadfarisi@mdp.ac.id>
2015-11-15 13:21:35 +0700

    4th commit

--- index.php
+++ index.php
@@ -1,22 +1,46 @@
 <html>
 	<head>
 		<title>Home | Azure Workshop</title>
+		<?php
+			if(!empty($_POST)){
+				$name  = $_POST["txtName"];
+				$email = $_POST["txtEmail"]; 
+			}
+		?>
 	</head>
 	
 	<body>
 		<fieldset>
-			<legend>Input Data</legend>
-				<form action="result.php" method="POST">
+			<legend>Data</legend>
+				<form action="" method="POST">
 					<table>
 						<tr>
 							<td>Name</td>
 							<td>:</td>
-							<td><input type="text" name="txtName" required/></td>
+							<td>
+								<?php
+									if(!empty($_POST)){
+										echo $name;
+									}
+									else {
+										echo "<input type='text' name='txtName' required/>";
+									}
+								?>
+							</td>
 						</tr>
 						<tr>
 							<td>Email</td>
 							<td>:</td>
-							<td><input type="text" name="txtEmail" required/></td>
+							<td>
+								<?php
+									if(!empty($_POST)){
+										echo $email;
+									}
+									else {
+										echo "<input type='text' name='txtEmail' required/>";
+									}
+								?>
+							</td>
 						</tr>
 						<tr>
 							<td>&nbsp;</td>
@@ -25,8 +49,15 @@
 						</tr>
 						<tr>
 							<td colspan="3">
-								<input type="reset" value="Reset"/>
-								<input type="submit" name="btnSubmit" value="Submit"/>
+								<?php
+									if(!empty($_POST)){
+										echo "<a href='index.php'>[ Back ]</a>";
+									}
+									else {
+										echo "	<input type='reset' value='Reset'/>
+												<input type='submit' value='Submit'/>";
+									}
+								?>
 							</td>
 						</tr>
 					</table>
