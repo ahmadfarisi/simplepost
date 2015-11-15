97bbe13a767b90f26426318fa0399066cceafbcb
ahmadfarisi <ahmadfarisi@mdp.ac.id>
2015-11-15 13:54:14 +0700

    6th commit

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 	$hostname = "br-cdbr-azure-south-a.cloudapp.net";
 	$username = "b2e09bd992e5c6";
 	$password = "1624023a";
-	$dbname   = "dbsimplepost";
+	$dbname   = "dbworkshopazure";
 	
 	$conn = mysql_connect($hostname, $username, $password) or die("MYSQL not Connected");
 	$db   = mysql_select_db($dbname) or die("Database is not found");
