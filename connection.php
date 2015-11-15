1bf8687c4c3b53d599624a0a49448e91c6d5e46c
ahmadfarisi <ahmadfarisi@mdp.ac.id>
2015-11-15 13:52:55 +0700

    5th commit

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+	$hostname = "br-cdbr-azure-south-a.cloudapp.net";
+	$username = "b2e09bd992e5c6";
+	$password = "1624023a";
+	$dbname   = "dbsimplepost";
+	
+	$conn = mysql_connect($hostname, $username, $password) or die("MYSQL not Connected");
+	$db   = mysql_select_db($dbname) or die("Database is not found");
+?>
--- index.php
+++ index.php
@@ -1,46 +1,24 @@
 <html>
 	<head>
 		<title>Home | Azure Workshop</title>
-		<?php
-			if(!empty($_POST)){
-				$name  = $_POST["txtName"];
-				$email = $_POST["txtEmail"]; 
-			}
-		?>
 	</head>
 	
 	<body>
 		<fieldset>
-			<legend>Data</legend>
+			<legend>Input Data</legend>
 				<form action="" method="POST">
 					<table>
 						<tr>
 							<td>Name</td>
 							<td>:</td>
 							<td>
-								<?php
-									if(!empty($_POST)){
-										echo $name;
-									}
-									else {
-										echo "<input type='text' name='txtName' required/>";
-									}
-								?>
+								<input type="text" name="txtName" required/>
 							</td>
 						</tr>
 						<tr>
 							<td>Email</td>
 							<td>:</td>
-							<td>
-								<?php
-									if(!empty($_POST)){
-										echo $email;
-									}
-									else {
-										echo "<input type='text' name='txtEmail' required/>";
-									}
-								?>
-							</td>
+							<td><input type="text" name="txtEmail" required/></td>
 						</tr>
 						<tr>
 							<td>&nbsp;</td>
@@ -49,19 +27,55 @@
 						</tr>
 						<tr>
 							<td colspan="3">
-								<?php
-									if(!empty($_POST)){
-										echo "<a href='index.php'>[ Back ]</a>";
-									}
-									else {
-										echo "	<input type='reset' value='Reset'/>
-												<input type='submit' value='Submit'/>";
-									}
-								?>
+								<input type="reset" value="Reset"/>
+								<input type="submit" name="btnSubmit" value="Submit"/>
 							</td>
 						</tr>
 					</table>
 				</form>
+				
+				<?php
+					include("connection.php");
+					if(!empty($_POST)){
+						$name  = $_POST["txtName"];
+						$email = $_POST["txtEmail"]; 
+						$date = date("Y-m-d");
+						
+						$sql_insert = "INSERT INTO tblsimplepost (name, email, date) VALUES ('$name', '$email', '$date')";
+						mysql_query($sql_insert) or die (mysql_error());
+						
+						echo "
+							<h3>Registered User</h3>
+							
+							<table border='1'>
+								<tr>
+									<th>No</th>
+									<th>Nama</th>
+									<th>Email</th>
+									<th>Tanggal</th>
+								</tr>
+								";
+						
+						$sql_select = "SELECT*FROM tblsimplepost";
+						$result = mysql_query($sql_select) or die(mysql_error());
+						$i=1;
+						while($row=mysql_fetch_array($result)){
+							echo "
+									<tr>
+										<td>$i</td>
+										<td>".$row['name']."</td>
+										<td>".$row['email']."</td>
+										<td>".$row['date']."</td>
+									</tr>
+							
+							";
+							$i=$i+1;
+						}
+							
+							echo "</table>";
+					}
+				?>
+				
 		</fieldset>
 	</body>
 </html>
